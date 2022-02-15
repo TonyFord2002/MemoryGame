081e23868b18ffef73d7cdbafdcea3deb7b45818
TonyFord2002 <97993837+TonyFord2002@users.noreply.github.com>
2022-02-14 22:21:21 -0500

    All pictures made and pseudocode on apps

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # TonyFord2002.github.io-MemoryGame
-Memory game for 2
+A memory game for 2.
+This is a game played between two people. Player 1 starts by fliping over two cards and see if they match.
+If they do then player one gets a point. If they dont then the cards get flipped back over and the second player 
+gets to flip two cards over and see if they match.
+
+
---
+++ app.js
@@ -0,0 +1,31 @@
+//Start game button
+
+
+//List instructions or skip instructions if playing again
+
+
+//Make gameboard
+
+
+//Make players
+
+
+//Display the upsidedown cards
+
+
+//Button click on two cards to turn over
+
+
+//Compare the two cards and check for a match
+
+
+//If a match increase score on that player and go again
+
+
+//If not a match then turn over cards its the other players turn
+
+
+//If all cards are gone compare scores and declare winner
+
+
+//Ask to play again
